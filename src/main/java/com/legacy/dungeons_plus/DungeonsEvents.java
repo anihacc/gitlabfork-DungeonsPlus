7a163178330a150af341b12ba456919950582e22
Bailey <wheresperry41@gmail.com>
2020-04-28 02:01:06 -0500

    Endermen that spawn in the End Ruins should be hostile towards players

--- src/main/java/com/legacy/dungeons_plus/DungeonsEvents.java
+++ src/main/java/com/legacy/dungeons_plus/DungeonsEvents.java
@@ -5,6 +5,9 @@ import com.legacy.structure_gel.entities.EntityAccessHelper;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,6 +24,11 @@ public class DungeonsEvents
 		if (event.getEntity().getType().equals(EntityType.HUSK) && Features.LEVIATHAN.getFirst().isPositionInStructure(event.getWorld(), event.getEntity().getPosition()))
 		{
 			EntityAccessHelper.setDeathLootTable((MobEntity) event.getEntity(), LEVIATHAN_HUSK);
+		}
+
+		if (event.getEntity() instanceof EndermanEntity && Features.END_RUINS.getFirst().isPositionInStructure(event.getWorld(), event.getEntity().getPosition()))
+		{
+			((EndermanEntity) event.getEntity()).targetSelector.addGoal(1, new NearestAttackableTargetGoal<>((EndermanEntity) event.getEntity(), PlayerEntity.class, true, false));
 		}
 	}
 }
