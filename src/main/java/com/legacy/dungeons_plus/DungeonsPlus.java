7e73d1bac2e57ffac34e146abf5b735c17665f2e
Silver_David <djbake101@gmail.com>
2020-06-26 01:20:00 -0400

    Ported all changes from master

--- src/main/java/com/legacy/dungeons_plus/DungeonsPlus.java
+++ src/main/java/com/legacy/dungeons_plus/DungeonsPlus.java
@@ -42,7 +42,7 @@ public class DungeonsPlus
 {
 	public static final String NAME = "Dungeons Plus";
 	public static final String MODID = "dungeons_plus";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public DungeonsPlus()
