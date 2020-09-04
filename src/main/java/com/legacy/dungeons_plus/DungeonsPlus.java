7b0cd02515e6d239be6579b579adcf303bc1e444
Silver_David <djbake101@gmail.com>
2020-09-03 23:34:25 -0400

    Updated with API changes

--- src/main/java/com/legacy/dungeons_plus/DungeonsPlus.java
+++ src/main/java/com/legacy/dungeons_plus/DungeonsPlus.java
@@ -17,8 +17,8 @@ import com.legacy.structure_gel.access_helpers.BiomeAccessHelper;
 import com.legacy.structure_gel.access_helpers.JigsawAccessHelper;
 import com.legacy.structure_gel.biome_dictionary.BiomeDictionary;
 import com.legacy.structure_gel.biome_dictionary.BiomeType;
+import com.legacy.structure_gel.registrars.StructureRegistrar;
 import com.legacy.structure_gel.util.RegistryHelper;
-import com.legacy.structure_gel.util.StructureRegistrar;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
@@ -64,7 +64,7 @@ public class DungeonsPlus
 	public void commonInit(final FMLCommonSetupEvent event)
 	{
 		for (Biome biome : ForgeRegistries.BIOMES.getValues())
-		{
+		{	
 			if (DPConfig.COMMON.tower.isBiomeAllowed(biome))
 				BiomeAccessHelper.addStructure(biome, Structures.TOWER.getStructureFeature());
 
@@ -89,7 +89,7 @@ public class DungeonsPlus
 
 	@Mod.EventBusSubscriber(modid = DungeonsPlus.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class Structures
-	{
+	{				
 		public static StructureRegistrar<VillageConfig, TowerStructure> TOWER;
 		public static StructureRegistrar<VillageConfig, BiggerDungeonStructure> BIGGER_DUNGEON;
 		public static StructureRegistrar<VillageConfig, LeviathanStructure> LEVIATHAN;
@@ -100,12 +100,12 @@ public class DungeonsPlus
 		public static void onRegistry(final RegistryEvent.Register<Structure<?>> event)
 		{
 			IForgeRegistry<Structure<?>> registry = event.getRegistry();
-			TOWER = RegistryHelper.handleRegistrar(registry, locate("tower"), new TowerStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), Decoration.SURFACE_STRUCTURES, TowerStructure.Piece::new, new VillageConfig(() -> TowerPools.ROOT, 7));
-			BIGGER_DUNGEON = RegistryHelper.handleRegistrar(registry, locate("bigger_dungeon"), new BiggerDungeonStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.biggerDungeon), Decoration.UNDERGROUND_STRUCTURES, BiggerDungeonStructure.Piece::new, new VillageConfig(() -> BiggerDungeonPools.ROOT, 7));
-			LEVIATHAN = RegistryHelper.handleRegistrar(registry, locate("leviathan"), new LeviathanStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), Decoration.SURFACE_STRUCTURES, LeviathanStructure.Piece::new, new VillageConfig(() -> LeviathanPools.ROOT, 7));
-			SNOWY_TEMPLE = RegistryHelper.handleRegistrar(registry, locate("snowy_temple"), new SnowyTempleStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), Decoration.SURFACE_STRUCTURES, SnowyTempleStructure.Piece::new, new VillageConfig(() -> SnowyTemplePools.ROOT, 7));
-			END_RUINS = RegistryHelper.handleRegistrar(registry, locate("end_ruins"), new EndRuinsStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), Decoration.SURFACE_STRUCTURES, EndRuinsStructure.Piece::new, new VillageConfig(() -> EndRuinsPools.ROOT, 7));
-
+			TOWER = RegistryHelper.handleRegistrar(StructureRegistrar.of(registry, locate("tower"), new TowerStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), TowerStructure.Piece::new, new VillageConfig(() -> TowerPools.ROOT, 7), Decoration.SURFACE_STRUCTURES));
+			BIGGER_DUNGEON = RegistryHelper.handleRegistrar(StructureRegistrar.of(registry, locate("bigger_dungeon"), new BiggerDungeonStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.biggerDungeon), BiggerDungeonStructure.Piece::new, new VillageConfig(() -> BiggerDungeonPools.ROOT, 7), Decoration.SURFACE_STRUCTURES));
+			LEVIATHAN = RegistryHelper.handleRegistrar(StructureRegistrar.of(registry, locate("leviathan"), new LeviathanStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), LeviathanStructure.Piece::new, new VillageConfig(() -> LeviathanPools.ROOT, 7), Decoration.SURFACE_STRUCTURES));
+			SNOWY_TEMPLE = RegistryHelper.handleRegistrar(StructureRegistrar.of(registry, locate("snowy_temple"), new SnowyTempleStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), SnowyTempleStructure.Piece::new, new VillageConfig(() -> SnowyTemplePools.ROOT, 7), Decoration.SURFACE_STRUCTURES));
+			END_RUINS = RegistryHelper.handleRegistrar(StructureRegistrar.of(registry, locate("end_ruins"), new EndRuinsStructure(VillageConfig.field_236533_a_, DPConfig.COMMON.tower), EndRuinsStructure.Piece::new, new VillageConfig(() -> EndRuinsPools.ROOT, 7), Decoration.SURFACE_STRUCTURES));
+			
 			JigsawAccessHelper.addIllagerStructures(TOWER.getStructure(), SNOWY_TEMPLE.getStructure());
 
 			TowerPools.init();
