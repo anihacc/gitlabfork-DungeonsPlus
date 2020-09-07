bd4dcf144743fdf72038ac1211d4bb3d7c19ff88
Silver_David <djbake101@gmail.com>
2020-09-07 10:52:30 -0400

    Updated configs to use new biome dictionary entries. Dungeons Plus now has Biomes O Plenty support

--- src/main/java/com/legacy/dungeons_plus/DPConfig.java
+++ src/main/java/com/legacy/dungeons_plus/DPConfig.java
@@ -27,11 +27,11 @@ public class DPConfig
 
 		public Common(ForgeConfigSpec.Builder builder)
 		{
-			this.tower = new StructureConfig(builder, "tower").probability(0.75D).spacing(25).offset(6).biomes(true, "#dungeons_plus:tower_biomes");
-			this.leviathan = new StructureConfig(builder, "leviathan").probability(1.0D).spacing(36).offset(8).biomes(true, "#dungeons_plus:leviathan_biomes");
-			this.snowyTemple = new StructureConfig(builder, "snowy_temple").probability(1.0D).spacing(36).offset(8).biomes(true, "#dungeons_plus:snowy_temple_biomes");
-			this.biggerDungeon = new StructureConfig(builder, "bigger_dungeon").probability(0.4D).spacing(12).offset(5).biomes(true, "#dungeons_plus:bigger_dungeon_biomes, !mushroom_fields, !mushroom_field_shore");
-			this.endRuins = new StructureConfig(builder, "end_ruins").probability(0.8D).spacing(24).offset(8).biomes(true, "#dungeons_plus:end_ruins_biomes");
+			this.tower = new StructureConfig(builder, "tower").probability(0.75D).spacing(25).offset(6).biomes(true, "#structure_gel:plains, #structure_gel:oak_forest, #structure_gel:dark_forest, #structure_gel:birch_forest, #structure_gel:mountain");
+			this.leviathan = new StructureConfig(builder, "leviathan").probability(1.0D).spacing(36).offset(8).biomes(true, "#structure_gel:desert");
+			this.snowyTemple = new StructureConfig(builder, "snowy_temple").probability(1.0D).spacing(36).offset(8).biomes(true, "#structure_gel:snowy_plains, #structure_gel:snowy_spruce_forest");
+			this.biggerDungeon = new StructureConfig(builder, "bigger_dungeon").probability(0.4D).spacing(12).offset(5).biomes(true, "#structure_gel:overworld, !mushroom_fields, !mushroom_field_shore");
+			this.endRuins = new StructureConfig(builder, "end_ruins").probability(0.8D).spacing(24).offset(8).biomes(true, "#structure_gel:outer_end_islands");
 		}
 	}
 }
